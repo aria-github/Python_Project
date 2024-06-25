9fbd3d068b8dbd08a7ffb5564a2c7bd2361e7514
aria-github <80567283+aria-github@users.noreply.github.com>
2024-06-25 10:32:03 +0700

    Add files via upload

---
+++ Quote Scraping Project/4.Quote_Scraping_Saved_Into_CSV.py
@@ -0,0 +1,58 @@
+import requests
+from bs4 import BeautifulSoup
+from time import sleep
+from csv import DictWriter      # pake DictWriter yah
+ 
+
+base_url = "http://quotes.toscrape.com"
+
+ 
+def scrape_quotes():
+
+    all_quotes = []
+    url = "/page/1"
+ 
+    while url:
+        res = requests.get(f"{base_url}{url}")
+        print(f"Now Scraping {base_url}{url}....")
+        soup = BeautifulSoup(res.text, "html.parser")
+        quotes = soup.find_all(class_="quote")
+
+        for quote in quotes:
+            all_quotes.append({
+                "text": quote.find(class_="text").get_text(),
+                "author": quote.find(class_="author").get_text(),
+                "bio-link": quote.find("a")["href"]
+            })
+    
+        next_btn = soup.find(class_="next")
+        url = next_btn.find("a")["href"] if next_btn else None
+
+        # sleep(2)
+
+    return all_quotes
+
+
+def write_quotes(quotes):
+
+    with open("4.quotes.csv", "w", encoding="utf-8") as file:
+        headers = ["text", "author", "bio-link"]
+        csv_writer = DictWriter(file, fieldnames=headers)
+        csv_writer.writeheader()
+
+        for quote in quotes:
+            csv_writer.writerow(quote)
+
+
+quotes = scrape_quotes()
+
+#print(quotes)
+
+write_quotes(quotes)
+
+
+
+# kalo gak ada encoding="utf-8" , bakal error
+# 'charmap' codec can't encode character '\u2032' in position 1: character maps to <undefined>
+# quotes saved in "4.quotes.csv" file
+
